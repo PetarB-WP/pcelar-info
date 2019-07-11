18e9c362661b04bcc355af9e5ca19f185db11451
PetarB <petarbogic85@gmail.com>
2019-07-11 16:27:47 +0200

    Remove border line

--- js/javascript.js
+++ js/javascript.js
@@ -10,3 +10,12 @@ function w3_open_navbar() {
     overlayBg.className = overlayBg.className.replace(" w3-show", "");
   }
 }
+
+function searchShow(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
