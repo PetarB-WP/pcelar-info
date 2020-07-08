6417d91cdf99682bdbaa27d39854847fd9245d32
PetarB <petarbogic85@gmail.com>
2020-07-09 00:10:00 +0200

    Completed part for responsive

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,14 @@ function w3_open_navbar() {
   var sideBar = document.getElementById("sideBar");
   var overlayBg = document.getElementById("overLay");
 
-  if (sideBar.className.indexOf("w3-show") == -1) {
-    sideBar.className += " w3-show";
+  if (sideBar.className.indexOf("sidebar-show") == -1) {
+    overlayBg.className = overlayBg.className.replace(" w3-hide", "");
     overlayBg.className += " w3-show";
+    sideBar.className += " sidebar-show";
   } else {
-    sideBar.className = sideBar.className.replace(" w3-show", "");
     overlayBg.className = overlayBg.className.replace(" w3-show", "");
+    overlayBg.className += " w3-hide";
+    sideBar.className = sideBar.className.replace(" sidebar-show", "");
   }
 }
 
